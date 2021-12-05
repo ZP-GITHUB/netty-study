25ebe30f89c32a1a70c4039ba20765f9a7c4ec1c
ZP <991475623@qq.com>
2021-12-05 11:06:41 +0800

    update

--- zp-netty-study-05/src/main/java/com/zp/MyServerHandler.java
+++ zp-netty-study-05/src/main/java/com/zp/MyServerHandler.java
@@ -1,7 +1,5 @@
 package com.zp;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.socket.SocketChannel;
@@ -11,7 +9,7 @@ import java.util.Date;
 
 /**
  * @author ZP
- * @date 2021/12/4.
+ * @date 2021/12/5.
  */
 public class MyServerHandler extends ChannelInboundHandlerAdapter {
 
@@ -21,15 +19,14 @@ public class MyServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         SocketChannel channel = (SocketChannel) ctx.channel();
+        System.out.println("链接报告开始");
         System.out.println("链接报告信息：有一客户端链接到本服务端");
         System.out.println("链接报告IP:" + channel.localAddress().getHostString());
         System.out.println("链接报告Port:" + channel.localAddress().getPort());
         System.out.println("链接报告完毕");
         //通知客户端链接建立成功
         String str = "通知客户端链接建立成功" + " " + new Date() + " " + channel.localAddress().getHostString() + "\r\n";
-        ByteBuf buf = Unpooled.buffer(str.getBytes().length);
-        buf.writeBytes(str.getBytes("GBK"));
-        ctx.writeAndFlush(buf);
+        ctx.writeAndFlush(str);
     }
 
     /**
@@ -44,12 +41,9 @@ public class MyServerHandler extends ChannelInboundHandlerAdapter {
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         //接收msg消息{与上一章节相比，此处已经不需要自己进行解码}
         System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + " 接收到消息：" + msg);
-        //通知客户端链消息发送成功
+        //通知客户端链消息发送成功｛不需要通过ByteBuf，可以直接发送字符串｝
         String str = "服务端收到：" + new Date() + " " + msg + "\r\n";
-        // 创建str字节数组长度的bytebuf
-        ByteBuf buf = Unpooled.buffer(str.getBytes().length);
-        buf.writeBytes(str.getBytes("GBK"));
-        ctx.writeAndFlush(buf);
+        ctx.writeAndFlush(str);
     }
 
     /**
@@ -60,4 +54,5 @@ public class MyServerHandler extends ChannelInboundHandlerAdapter {
         ctx.close();
         System.out.println("异常信息：\r\n" + cause.getMessage());
     }
+
 }
