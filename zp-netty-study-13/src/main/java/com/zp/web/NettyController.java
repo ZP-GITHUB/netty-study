b28903fcb929ac635d7adcc72cf8304196460dc6
ZP <991475623@qq.com>
2021-12-18 23:08:04 +0800

    update

---
+++ zp-netty-study-13/src/main/java/com/zp/web/NettyController.java
@@ -0,0 +1,31 @@
+package com.zp.web;
+
+import com.zp.server.NettyServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author ZP
+ * @date 2021/12/18.
+ */
+@RestController
+@RequestMapping(value = "/nettyserver", method = RequestMethod.GET)
+public class NettyController {
+
+    @Resource
+    private NettyServer nettyServer;
+
+    @RequestMapping("/localAddress")
+    public String localAddress() {
+        return "nettyServer localAddress " + nettyServer.getChannel().localAddress();
+    }
+
+    @RequestMapping("/isOpen")
+    public String isOpen() {
+        return "nettyServer isOpen " + nettyServer.getChannel().isOpen();
+    }
+
+}
