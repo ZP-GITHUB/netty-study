50c38f54a9c100ce0066a8b5d0273163e64a221c
ZP <991475623@qq.com>
2021-12-24 23:52:39 +0800

    update

---
+++ zp-netty-study-14/src/main/java/com/zp/client/MyChannelInitializer.java
@@ -0,0 +1,23 @@
+package com.zp.client;
+
+import com.zp.codec.ObjDecoder;
+import com.zp.codec.ObjEncoder;
+import com.zp.domain.MsgInfo;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+/**
+ * @author ZP
+ * @date 2021/12/24.
+ */
+public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel channel) throws Exception {
+        //对象传输处理
+        channel.pipeline().addLast(new ObjDecoder(MsgInfo.class));
+        channel.pipeline().addLast(new ObjEncoder(MsgInfo.class));
+        // 在管道中添加我们自己的接收数据实现方法
+        channel.pipeline().addLast(new MyClientHandler());
+    }
+}
