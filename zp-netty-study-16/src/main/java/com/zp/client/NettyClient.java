47b35a18a5be8bff20f0267aac160f6b8248441e
ZP <991475623@qq.com>
2021-12-27 22:28:11 +0800

    update

---
+++ zp-netty-study-16/src/main/java/com/zp/client/NettyClient.java
@@ -0,0 +1,38 @@
+package com.zp.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @author ZP
+ * @date 2021/12/27.
+ */
+public class NettyClient {
+
+    public static void main(String[] args) {
+        new NettyClient().connect("192.168.1.101", 7397);
+    }
+
+    public void connect(String inetHost, int inetPort) {
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(workerGroup);
+            b.channel(NioSocketChannel.class);
+            b.option(ChannelOption.AUTO_READ, true);
+            b.handler(new MyChannelInitializer());
+            ChannelFuture f = b.connect(inetHost, inetPort).sync();
+            f.addListener(new MyChannelFutureListener()); //添加监听，处理重连
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+}
