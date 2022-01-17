7c182f2e43d6ff6b5a41b73f5cd24ef641226fcf
ZP <991475623@qq.com>
2022-01-17 22:18:43 +0800

    update

---
+++ zp-netty-study-22/zp-rpc-study-03/src/main/java/com/zp/rpc/config/spring/bean/ConsumerBean.java
@@ -0,0 +1,58 @@
+package com.zp.rpc.config.spring.bean;
+
+/**
+ * @author ZP
+ * @date 2022/1/17.
+ */
+public class ConsumerBean<T> extends ConsumerConfig<T> implements FactoryBean {
+
+    private ChannelFuture channelFuture;
+
+    private RpcProviderConfig rpcProviderConfig;
+
+    @Override
+    public Object getObject() throws Exception {
+
+        //从redis获取链接
+        if (null == rpcProviderConfig) {
+            String infoStr = RedisRegistryCenter.obtainProvider(nozzle, alias);
+            rpcProviderConfig = JSON.parseObject(infoStr, RpcProviderConfig.class);
+        }
+        Assert.isTrue(null != rpcProviderConfig);
+
+        //获取通信channel
+        if (null == channelFuture) {
+            ClientSocket clientSocket = new ClientSocket(rpcProviderConfig.getHost(), rpcProviderConfig.getPort());
+            new Thread(clientSocket).start();
+            for (int i = 0; i < 100; i++) {
+                if (null != channelFuture) break;
+                Thread.sleep(500);
+                channelFuture = clientSocket.getFuture();
+            }
+        }
+        Assert.isTrue(null != channelFuture);
+
+        Request request = new Request();
+        request.setChannel(channelFuture.channel());
+        request.setNozzle(nozzle);
+        request.setRef(rpcProviderConfig.getRef());
+        request.setAlias(alias);
+        return (T) JDKProxy.getProxy(ClassLoaderUtils.forName(nozzle), request);
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        try {
+            return ClassLoaderUtils.forName(nozzle);
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+
+
+}
---
+++ zp-netty-study-22/zp-rpc-study-03/src/main/java/com/zp/rpc/config/spring/bean/ProviderBean.java
@@ -0,0 +1,27 @@
+package com.zp.rpc.config.spring.bean;
+
+/**
+ * @author ZP
+ * @date 2022/1/17.
+ */
+public class ProviderBean extends ProviderConfig implements ApplicationContextAware {
+
+    private Logger logger = LoggerFactory.getLogger(ProviderBean.class);
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+        RpcProviderConfig rpcProviderConfig = new RpcProviderConfig();
+        rpcProviderConfig.setNozzle(nozzle);
+        rpcProviderConfig.setRef(ref);
+        rpcProviderConfig.setAlias(alias);
+        rpcProviderConfig.setHost(LocalServerInfo.LOCAL_HOST);
+        rpcProviderConfig.setPort(LocalServerInfo.LOCAL_PORT);
+
+        //注册生产者
+        long count = RedisRegistryCenter.registryProvider(nozzle, alias, JSON.toJSONString(rpcProviderConfig));
+
+        logger.info("注册生产者：{} {} {}", nozzle, alias, count);
+    }
+
+}
